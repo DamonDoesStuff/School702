c622eaeae2f197f163082c74725f7ae86c6d06bd
DamonRochon <>
2021-06-08 14:20:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let userguess = 0
+
+document.getElementById('button').addEventListener('click', comparison)
+
+function comparison() {
+  userguess = document.getElementById('lost').value
+  userguess = parseInt(userguess)
+
+  if (userguess < 0) {
+    document.getElementById('steve').innerHTML = "The Number is Negative!!!"
+  }
+    else {
+    document.getElementById('steve').innerHTML = "The Number is Positive!!!"
+  }
+}
