2cdd7157dd3d005c3ccd55c202e7253b9b58fcbb
Damon Rochon <>
2021-06-08 14:26:43 +0000

    '''

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let userguess = 0
 
 document.getElementById('button').addEventListener('click', comparison)
 
-function comparison() {
+function comparison () {
   userguess = document.getElementById('lost').value
   userguess = parseInt(userguess)
 
   if (userguess < 0) {
-    document.getElementById('steve').innerHTML = "The Number is Negative!!!"
+    document.getElementById('steve').innerHTML = 'The Number is Negative!!!'
   }
     else {
-    document.getElementById('steve').innerHTML = "The Number is Positive!!!"
+    document.getElementById('steve').innerHTML = 'The Number is Positive!!!'
   }
 }
